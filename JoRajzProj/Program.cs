84390239ad8d9d2f86ba68f6a13948a0989d6df3
szabo.akos.csaba <szabo.akos.csaba@blathy.info>
2024-10-03 11:13:03 +0200

    Add project files.

---
+++ JoRajzProj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JoRajzProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
